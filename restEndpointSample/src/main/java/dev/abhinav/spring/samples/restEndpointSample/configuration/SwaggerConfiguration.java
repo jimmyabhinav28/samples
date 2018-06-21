8fd666aca4be98b62f2ab4b520e75f3c47c30271
abhinavPTM <abhinav.anand@paytmmall.com>
2018-06-21 11:52:49 +0530

    Swagger UI up and running

--- restEndpointSample/src/main/java/dev/abhinav/spring/samples/restEndpointSample/configuration/SwaggerConfiguration.java
+++ restEndpointSample/src/main/java/dev/abhinav/spring/samples/restEndpointSample/configuration/SwaggerConfiguration.java
@@ -26,38 +26,47 @@ public class SwaggerConfiguration extends WebMvcConfigurationSupport {
 	 */
 	@Bean
 	public Docket productApi() {
-//		return new Docket(DocumentationType.SWAGGER_2).select()
-//				.apis(RequestHandlerSelectors.basePackage("dev.abhinav.spring.samples.restEndpointSample.controllers"))
-//				.paths(regex("/product.*")).build().apiInfo(metaData());
-		
-		//we had configured to expose only controllers in package 
-		//dev.abhinav.spring.samples.restEndpointSample.controllers
-		//to be exposed by swagger in the code commented above
-		
-		//below we have instructed that all request handlers must be exposed by 
-		//swagger
-		//also the paths selector tells that api's corresponding to which path must be
-		//exposed by Swagger. Below we have instructed swagger to select all paths
-		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any()).build()
-				.apiInfo(metaData());
+		/*
+		 * return new Docket(DocumentationType.SWAGGER_2).select()
+		 * .apis(RequestHandlerSelectors.basePackage(
+		 * "dev.abhinav.spring.samples.restEndpointSample.controllers"))
+		 * .paths(regex("/product.*")).build().apiInfo(metaData());
+		 */
+
+		/*
+		 * we had configured to expose only controllers in package
+		 * dev.abhinav.spring.samples.restEndpointSample.controllers to be exposed by
+		 * swagger in the code commented above
+		 * 
+		 * below we have instructed that all request handlers must be exposed by swagger
+		 * Also the paths selector tells that api's corresponding to which path must be
+		 * exposed by Swagger. Below we have instructed swagger to select all paths
+		 */
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+				// .paths(regex("/samples.*")).build();
+				.paths(PathSelectors.any()).build().apiInfo(metaData());
 	}
 
-	//add metadata about the apis here
+	/**
+	 * add metadata about api's here. This is purely optional
+	 * 
+	 * @return
+	 */
 	private ApiInfo metaData() {
-		return new ApiInfoBuilder().title("Spring Boot REST API")
-				.description("\"Spring Boot REST API for Online Store\"").version("1.0.0")
-				.license("Apache License Version 2.0").licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
-				.contact(
-						new Contact("John Thompson", "https://springframework.guru/about/", "john@springfrmework.guru"))
+		return new ApiInfoBuilder().title("Spring Boot REST API").description("\"Spring Boot demo REST API \"")
+				.version("1.0.0").license("Apache License Version 2.0")
+				.licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"").contact(new Contact("Abhinav Anand",
+						"https://www.linkedin.com/in/abhinav-anand-0b970221/", "jimmyabhinav28@gmail.com"))
 				.build();
 	}
 
-//	@Override
-//	protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-//		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
-//
-//		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
-//	}
+	/**
+	 * One has to add these resource handlers for swagger-ui to show up
+	 */
+	@Override
+	protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
 }
--- restEndpointSample/src/main/java/dev/abhinav/spring/samples/restEndpointSample/controllers/SampleController.java
+++ restEndpointSample/src/main/java/dev/abhinav/spring/samples/restEndpointSample/controllers/SampleController.java
@@ -1,11 +1,9 @@
 package dev.abhinav.spring.samples.restEndpointSample.controllers;
 
-
 import java.util.ArrayList;
 import java.util.Map;
 
 import org.springframework.http.HttpHeaders;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,78 +22,79 @@ import io.swagger.annotations.ApiResponses;
 
 @RestController
 @RequestMapping("/samples")
-public class SampleController
-{
-	
-	@ApiOperation(value = "",response = SampleGetCallResponseModel.class)
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Successfully retrieved list"),
-//            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
-//            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
-//            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
-    })
-//	Added the @ApiResponses()
-	@GetMapping(path="/getSampleEndpoint/{pathParameter1}/{pathParameter2}")
-	public SampleGetCallResponseModel showAllParametersInGetRequest(@RequestHeader HttpHeaders requestHeaders,@RequestParam Map<String,String> allQueryParameters, @PathVariable String pathParameter1,@PathVariable String pathParameter2)
-	{
-		SampleGetCallResponseModel response=new SampleGetCallResponseModel();
-		ArrayList<Parameter> queryParameters=new ArrayList<Parameter>();
-		ArrayList<Parameter> headers=new ArrayList<Parameter>();
-		ArrayList<Parameter> pathParameters=new ArrayList<Parameter>();
-		
+public class SampleController {
+
+	@ApiOperation(value = "", response = SampleGetCallResponseModel.class)
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Successfully retrieved list"),
+			// @ApiResponse(code = 401, message = "You are not authorized to view the
+			// resource"),
+			// @ApiResponse(code = 403, message = "Accessing the resource you were trying to
+			// reach is forbidden"),
+			// @ApiResponse(code = 404, message = "The resource you were trying to reach is
+			// not found")
+	})
+	// Added the @ApiResponses()
+	@GetMapping(path = "/getSampleEndpoint/{pathParameter1}/{pathParameter2}")
+	public SampleGetCallResponseModel showAllParametersInGetRequest(@RequestHeader HttpHeaders requestHeaders,
+			@RequestParam Map<String, String> allQueryParameters, @PathVariable String pathParameter1,
+			@PathVariable String pathParameter2) {
+		SampleGetCallResponseModel response = new SampleGetCallResponseModel();
+		ArrayList<Parameter> queryParameters = new ArrayList<Parameter>();
+		ArrayList<Parameter> headers = new ArrayList<Parameter>();
+		ArrayList<Parameter> pathParameters = new ArrayList<Parameter>();
+
 		pathParameters.add(new Parameter("pathParameter1", pathParameter1));
 		pathParameters.add(new Parameter("pathParameter2", pathParameter2));
-		
-		for(String queryParameter: allQueryParameters.keySet())
-		{
-			String queryParameterValue=allQueryParameters.get(queryParameter);
+
+		for (String queryParameter : allQueryParameters.keySet()) {
+			String queryParameterValue = allQueryParameters.get(queryParameter);
 			queryParameters.add(new Parameter(queryParameter, queryParameterValue));
 		}
-		
-		for(String header: requestHeaders.keySet())
-		{
-			String headerValue=requestHeaders.get(header).toString();
+
+		for (String header : requestHeaders.keySet()) {
+			String headerValue = requestHeaders.get(header).toString();
 			headers.add(new Parameter(header, headerValue));
 		}
-		
+
 		response.setHeaders(headers);
 		response.setPathParameters(pathParameters);
 		response.setQueryParameters(queryParameters);
-		
+
 		return response;
 	}
-	
-	@ApiOperation(value = "",response = SampleGetCallResponseModel.class)
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Successfully retrieved list"),
-//            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
-//            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
-//            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
-    })
-//	Added the @ApiResponses()
-	@PostMapping(path="/postSampleEndpoint/{pathParameter1}/{pathParameter2}")
-	public SamplePostCallResponseModel showAllParametersInPostRequest(@RequestHeader HttpHeaders requestHeaders,@RequestParam Map<String,String> allQueryParameters,@PathVariable String pathParameter1,@PathVariable String pathParameter2, @RequestBody String requestBody)
-	{
-		SamplePostCallResponseModel response=new SamplePostCallResponseModel();
-		ArrayList<Parameter> queryParameters=new ArrayList<Parameter>();
-		ArrayList<Parameter> headers=new ArrayList<Parameter>();
-		ArrayList<Parameter> pathParameters=new ArrayList<Parameter>();
-		
+
+	@ApiOperation(value = "", response = SampleGetCallResponseModel.class)
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Successfully parsed all parameters"),
+			// @ApiResponse(code = 401, message = "You are not authorized to view the
+			// resource"),
+			// @ApiResponse(code = 403, message = "Accessing the resource you were trying to
+			// reach is forbidden"),
+			// @ApiResponse(code = 404, message = "The resource you were trying to reach is
+			// not found")
+	})
+	// Added the @ApiResponses()
+	@PostMapping(path = "/postSampleEndpoint/{pathParameter1}/{pathParameter2}")
+	public SamplePostCallResponseModel showAllParametersInPostRequest(@RequestHeader HttpHeaders requestHeaders,
+			@RequestParam Map<String, String> allQueryParameters, @PathVariable String pathParameter1,
+			@PathVariable String pathParameter2, @RequestBody String requestBody) {
+		SamplePostCallResponseModel response = new SamplePostCallResponseModel();
+		ArrayList<Parameter> queryParameters = new ArrayList<Parameter>();
+		ArrayList<Parameter> headers = new ArrayList<Parameter>();
+		ArrayList<Parameter> pathParameters = new ArrayList<Parameter>();
+
 		pathParameters.add(new Parameter("pathParameter1", pathParameter1));
 		pathParameters.add(new Parameter("pathParameter2", pathParameter2));
-		
-		for(String queryParameter: allQueryParameters.keySet())
-		{
-			String queryParameterValue=allQueryParameters.get(queryParameter);
+
+		for (String queryParameter : allQueryParameters.keySet()) {
+			String queryParameterValue = allQueryParameters.get(queryParameter);
 			queryParameters.add(new Parameter(queryParameter, queryParameterValue));
 		}
-		
-		for(String header: requestHeaders.keySet())
-		{
-			String headerValue=requestHeaders.get(header).toString();
+
+		for (String header : requestHeaders.keySet()) {
+			String headerValue = requestHeaders.get(header).toString();
 			headers.add(new Parameter(header, headerValue));
 		}
-		
+
 		response.setHeaders(headers);
 		response.setPathParameters(pathParameters);
 		response.setQueryParameters(queryParameters);
